1870a12a37abc92ce0addd7399073e35b8798e41
TheBlaze <santi.volcanos@gmail.com>
2025-07-11 13:51:25 -0600

    script wasnt saved

--- Assets/Scripts/Tamagotchi/Tamagotchi.cs
+++ Assets/Scripts/Tamagotchi/Tamagotchi.cs
@@ -1,7 +1,15 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Tamagotchi : MonoBehaviour
 {
+    float hunger=30; //0.00347222222 para modo realista
+    float tiredness=30;//0.00277777777 para modo realista
+    float boredom = 30;//0.01388888888 para modo realista
+    GameObject[] kirbystates;// 0 Feliz, 1 triste, 2 aburrido 3 enojado 4 con sueño
+                             
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,30 @@ public class Tamagotchi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hunger < 100)
+        {
+            hunger += Time.deltaTime * .1f;
+            if (hunger > 100)
+            { hunger = 100; }
+        }
+        else
+        {
+            
+        }
+
+
+        if (tiredness < 100)
+        {
+            tiredness += Time.deltaTime * .1f;
+            if (tiredness > 100)
+            { tiredness = 100; }
+        }
+
+        if (boredom < 100)
+        {
+            boredom += Time.deltaTime * .1f;
+            if (boredom > 100)
+            { boredom = 100; }
+        }
     }
 }
