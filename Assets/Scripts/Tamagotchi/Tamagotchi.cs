ee31f8980b650a470e444f05071396519cd429b0
TheBlaze <santi.volcanos@gmail.com>
2025-07-11 15:05:32 -0600

    KirbyTomodatchi UI Progress

--- Assets/Scripts/Tamagotchi/Tamagotchi.cs
+++ Assets/Scripts/Tamagotchi/Tamagotchi.cs
@@ -6,6 +6,7 @@ public class Tamagotchi : MonoBehaviour
     float hunger=30; //0.00347222222 para modo realista
     float tiredness=30;//0.00277777777 para modo realista
     float boredom = 30;//0.01388888888 para modo realista
+    [SerializeField]
     GameObject[] kirbystates;// 0 Feliz, 1 triste, 2 aburrido 3 enojado 4 con sueño
                              
 
@@ -22,13 +23,12 @@ public class Tamagotchi : MonoBehaviour
         if (hunger < 100)
         {
             hunger += Time.deltaTime * .1f;
+            if (hunger > 50)
+                kirbystates[2].SetActive(true);
             if (hunger > 100)
             { hunger = 100; }
         }
-        else
-        {
-            
-        }
+
 
 
         if (tiredness < 100)
@@ -43,6 +43,18 @@ public class Tamagotchi : MonoBehaviour
             boredom += Time.deltaTime * .1f;
             if (boredom > 100)
             { boredom = 100; }
+        }
+        else if (kirbystates[1].activeSelf && kirbystates[2].activeSelf && kirbystates[4].activeSelf)
+        {
+            kirbystates[1].SetActive(false);
+            kirbystates[2].SetActive(false);
+            kirbystates[4].SetActive(false);
+            kirbystates[3].SetActive(true);
+        }
+        else
+        {
+            kirbystates[3].SetActive(false);
+
         }
     }
 }
